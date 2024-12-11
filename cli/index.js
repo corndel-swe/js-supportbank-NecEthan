692ca2e1665c28fa058145e92753c6a62c7284d9
GLOBAL\ethan.wicks <ethan.wicks@necsws.com>
2024-12-11 10:46:42 +0000

    add

---
+++ cli/index.js
@@ -0,0 +1,6 @@
+import { program } from 'commander'
+import transactionController from './transaction.js'
+
+program.version('0.1.0').description('SupportBank')
+program.addCommand(transactionController)
+program.parse(process.argv)
--- cli/transaction.js
+++ cli/transaction.js
@@ -6,7 +6,7 @@ transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
-    // TODO
+    console.log(`At ${from} Jon A sent ${to} £${amount}`)
   })
 
 export default transactionController
